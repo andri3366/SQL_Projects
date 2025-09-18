acdc27e930ed931fbe0c5a4d1ecbb4f367f4e45e
andri3366 <AndriannaM17@gmail.com>
2025-09-18 11:48:28 -0400

    Update MegaEv

--- Pokemon_Project/Scripts/MegaEv.sql
+++ Pokemon_Project/Scripts/MegaEv.sql
@@ -5,7 +5,57 @@ SELECT
     mega.nameChange     AS mega_name,
     base."Legendary"	AS legendary,
     (base."HP" + base."Attack" + base."Defense" + base."SpAtk" + base."SpDef" + base."Speed") AS base_stat,
-    (mega."HP" + mega."Attack" + mega."Defense" + mega."SpAtk" + mega."SpDef" + mega."Speed") AS mega_stat
+    (mega."HP" + mega."Attack" + mega."Defense" + mega."SpAtk" + mega."SpDef" + mega."Speed") AS mega_stat,
+    (mega."HP" - base."HP") AS hp_gain,
+    (mega."Attack" - base."Attack") AS attack_gain,
+    (mega."Defense" - base."Defense") AS defense_gain,
+    (mega."SpAtk" - base."SpAtk") AS spatk_gain,
+    (mega."SpDef" - base."SpDef") AS spdef_gain,
+    (mega."Speed" - base."Speed") AS speed_gain,
+    CASE
+    	WHEN (mega."HP" - base."HP") = GREATEST(
+    		(mega."HP" - base."HP"),
+    		(mega."Attack" - base."Attack"),
+    		(mega."Defense" - base."Defense"),
+    		(mega."SpAtk" - base."SpAtk"),
+   			(mega."SpDef" - base."SpDef"),
+    		(mega."Speed" - base."Speed") 
+    	) THEN 'HP'
+    	WHEN (mega."Attack" - base."Attack") = GREATEST(
+    		(mega."HP" - base."HP"),
+    		(mega."Attack" - base."Attack"),
+    		(mega."Defense" - base."Defense"),
+    		(mega."SpAtk" - base."SpAtk"),
+   			(mega."SpDef" - base."SpDef"),
+    		(mega."Speed" - base."Speed") 
+    	) THEN 'Attack'
+    	WHEN (mega."Defense" - base."Defense") = GREATEST(
+    		(mega."HP" - base."HP"),
+    		(mega."Attack" - base."Attack"),
+    		(mega."Defense" - base."Defense"),
+    		(mega."SpAtk" - base."SpAtk"),
+   			(mega."SpDef" - base."SpDef"),
+    		(mega."Speed" - base."Speed") 
+    	) THEN 'Defense'
+    	WHEN (mega."SpAtk" - base."SpAtk") = GREATEST(
+    		(mega."HP" - base."HP"),
+    		(mega."Attack" - base."Attack"),
+    		(mega."Defense" - base."Defense"),
+    		(mega."SpAtk" - base."SpAtk"),
+   			(mega."SpDef" - base."SpDef"),
+    		(mega."Speed" - base."Speed") 
+    	) THEN 'SpAtk'
+    	WHEN (mega."SpDef" - base."SpDef") = GREATEST(
+    		(mega."HP" - base."HP"),
+    		(mega."Attack" - base."Attack"),
+    		(mega."Defense" - base."Defense"),
+    		(mega."SpAtk" - base."SpAtk"),
+   			(mega."SpDef" - base."SpDef"),
+    		(mega."Speed" - base."Speed") 
+    	) THEN 'SpDef'
+    	ELSE 'Speed'
+    END AS biggest_gain
+    
 FROM pokemon_analysis base
 --JOIN pokemon_analysis mega ON mega.nameChange LIKE 'Mega ' || base.nameChange || '%'
 JOIN pokemon_analysis mega 
@@ -20,12 +70,14 @@ SELECT DISTINCT ON (base_name)
     legendary,
     base_stat,
     mega_stat,
-	(mega_stat - base_stat) AS stat_remainder
+	(mega_stat - base_stat) AS stat_remainder,
+	ROUND(((mega_stat - base_stat) * 100 / base_stat), 2) AS pct_increase,
+	biggest_gain
 FROM mega_evol
 ORDER BY base_name, stat_remainder DESC
 )
 SELECT 
 	*
 FROM stronger_mega
-ORDER BY mega_stat DESC, base_stat
+ORDER BY mega_stat DESC, base_stat, pct_increase
 
